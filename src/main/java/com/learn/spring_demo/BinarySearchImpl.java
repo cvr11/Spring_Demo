e64fd8a29b343b650b94845d7a63924ab63efd47
LAPTOP-452QGQ9Q\rvc41 <venkat90001@gmail.com>
2018-06-17 13:06:51 -0700

    .

--- src/main/java/com/learn/spring_demo/BinarySearchImpl.java
+++ src/main/java/com/learn/spring_demo/BinarySearchImpl.java
@@ -1,5 +1,12 @@
 package com.learn.spring_demo;
 
 public class BinarySearchImpl {
+	
+	public int binarysearch(int[] numbers, int numbersToSearchFor ) { 
+	// Implementing sorting Array
+	// search the array
+	// return the result
+	return 3;
+	}
 
 }
